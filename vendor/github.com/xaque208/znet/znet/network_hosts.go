638c244efc23baa2d3b591a221d2a7f8e6c787d6
Zach Leslie <xaque208@gmail.com>
2019-11-03 21:25:31 -0800

    Vendor update

--- vendor/github.com/xaque208/znet/znet/network_hosts.go
+++ vendor/github.com/xaque208/znet/znet/network_hosts.go
@@ -162,3 +162,80 @@ func (z *Znet) NetworkHosts() ([]NetworkHost, error) {
 
 	return hosts, nil
 }
+
+func (h *NetworkHost) Update() (*ssh.Conn, error) {
+
+	sshConfig := &ssh.ClientConfig{
+		User: "zach",
+		Auth: []ssh.AuthMethod{
+			PublicKeyFile("/home/zach/.ssh/id_ed25519"),
+			SSHAgent(),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	log.Warnf("%+v", sshConfig)
+
+	connection, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", h.HostName, 22), sshConfig)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to dial: %s", err)
+	}
+
+	// session, err := connection.NewSession()
+	// if err != nil {
+	// 	return nil, fmt.Errorf("Failed to create session: %s", err)
+	// }
+	//
+	// stdin, err := session.StdinPipe()
+	// if err != nil {
+	// 	return nil, fmt.Errorf("Unable to setup stdin for session: %v", err)
+	// }
+	// go io.Copy(stdin, os.Stdin)
+	//
+	// stdout, err := session.StdoutPipe()
+	// if err != nil {
+	// 	return nil, fmt.Errorf("Unable to setup stdout for session: %v", err)
+	// }
+	// go io.Copy(os.Stdout, stdout)
+	//
+	// stderr, err := session.StderrPipe()
+	// if err != nil {
+	// 	return nil, fmt.Errorf("Unable to setup stderr for session: %v", err)
+	// }
+	// go io.Copy(os.Stderr, stderr)
+	//
+	// err = session.Run("ls -l")
+	// if err != nil {
+	// 	log.Error(err)
+	// }
+	//
+	// out, err := session.CombinedOutput("ls -l")
+	// if err != nil {
+	// 	log.Error(err)
+	// }
+	//
+	// log.Info(out)
+	log.Infof("Connection: %+v", connection)
+
+	return nil, nil
+}
+
+func SSHAgent() ssh.AuthMethod {
+	if sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+		return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
+	}
+	return nil
+}
+
+func PublicKeyFile(file string) ssh.AuthMethod {
+	buffer, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil
+	}
+
+	key, err := ssh.ParsePrivateKey(buffer)
+	if err != nil {
+		return nil
+	}
+	return ssh.PublicKeys(key)
+}
